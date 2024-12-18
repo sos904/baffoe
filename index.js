c6088058a38867cb4a52082bfcf7a6dcb159f4cf
sos904 <85062717+sos904@users.noreply.github.com>
2024-12-18 12:13:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -53,6 +53,5 @@ app.post("/send-sms", async (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () =>
-  console.log(`Proxy server running on http://localhost:${PORT}`)
-);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Proxy server running on port ${PORT}`));
