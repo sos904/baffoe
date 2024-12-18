d49ac05dad459b498d8e5a7226173a00f3e6a662
sos904 <85062717+sos904@users.noreply.github.com>
2024-12-18 11:49:33 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const axios = require("axios");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post("/send-sms", async (req, res) => {
   try {
